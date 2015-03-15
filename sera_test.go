44c22528805cf1a864a79fe95c9b85d4da6a30f0
Stig Lindqvist <stojg.lindqvist@gmail.com>
2015-03-16 12:23:23 +1300

    test timeoutArg()

--- sera_test.go
+++ sera_test.go
@@ -2,7 +2,40 @@ package main
 
 import (
     "testing"
+    "os"
 )
+
+func TestTimeoutArgWorks(t *testing.T) {
+    oldArgs := os.Args
+    defer func() { os.Args = oldArgs }()
+    os.Args = []string{"sera", "10", "ls"}
+
+    expected := 10
+    var actual int
+    timeoutArg(&actual)
+
+    if actual != expected {
+        t.Errorf("got '%v', expected '%v'", actual, expected)
+    }
+}
+
+func TestTimeoutArgFailsOnNonInt(t *testing.T) {
+    oldArgs := os.Args
+    defer func() { os.Args = oldArgs }()
+    os.Args = []string{"sera", "fail", "ls"}
+
+    expected := 0
+    var actual int
+    err := timeoutArg(&actual)
+
+    if err == nil {
+        t.Errorf("expected error")
+    }
+
+    if actual != expected {
+        t.Errorf("got '%v', expected '%v'", actual, expected)
+    }
+}
 
 func TestMd5Generation(t *testing.T) {
     input :=  "ls -ltra"
